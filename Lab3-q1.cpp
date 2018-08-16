8f931842f15e3ace298aea96c5251d0050faca54
arunima18 <42172417+arunima18@users.noreply.github.com>
2018-08-16 20:57:02 +0530

    Add files via upload

---
+++ Lab3-q1.cpp
@@ -0,0 +1,48 @@
+//Define the library
+#include<iostream>
+
+using namespace std;
+
+//The main function
+int main(){
+
+//Declare the datatype
+int a;
+
+//Assign value to variable
+a=3;
+
+char b;
+b='s';
+
+bool c;
+c=false;
+
+float d;
+d=1.23566;
+
+double f;
+f=6.75866325;
+
+//Print the values of variables
+
+cout<<"a="<<a<<endl;
+cout<<"b="<<b<<endl;
+cout<<"c"<<c<<endl;
+cout<<"d="<<d<<endl;
+cout<<"f"<<f<<endl;
+
+//Print the sizes of variables
+
+cout<<"The Size of a is "<<sizeof(a)<<endl;
+cout<<"The Size of b is "<<sizeof(b)<<endl;
+cout<<"The Size of c is "<<sizeof(c)<<endl;
+cout<<"The Sizo of d is "<<sizeof(d)<<endl;
+cout<<"The Size of f is "<<sizeof(f)<<endl;
+
+//Return an integer value
+
+return 9;
+}
+
+ 
